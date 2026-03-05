cf98a9b27284a7d217223299f98f18a568b3a52e
yegi318 <pocke0318@gmail.com>
2026-03-05 18:30:15 +0900

    Add files via upload

---
+++ player.js
@@ -0,0 +1,50 @@
+const songs = [
+  { 
+    title: "🎵 Still 3PM", 
+    file: "만약 네가 4시에 온다면 Still 3PM.mp3", 
+    cover: "https://i.pinimg.com/originals/94/c2/45/94c2451c86aa613788e342a6e48312af.gif"
+  },
+  { 
+    title: "🎵 Steady", 
+    file: "NCT WISH (엔시티 위시) 'Steady' Official Audio.mp3", 
+    cover: "https://i.pinimg.com/originals/3f/07/6b/3f076b45a3159f8d4c30ae3578847c9c.gif"
+  },
+  { 
+    title: "🎵 NASA", 
+    file: "NASA.mp3", 
+    cover: "https://i.pinimg.com/originals/54/47/4e/54474e4604471b331996ee9bffb4019f.gif"
+  }
+];
+
+let currentIndex = 0;
+
+function loadSong(index) {
+  document.getElementById("title").textContent = songs[index].title;
+  document.getElementById("audio").src = songs[index].file;
+  document.getElementById("cover").src = songs[index].cover;
+  document.getElementById("audio").play();
+}
+
+function nextSong() {
+  currentIndex = (currentIndex + 1) % songs.length;
+  loadSong(currentIndex);
+}
+
+function prevSong() {
+  currentIndex = (currentIndex - 1 + songs.length) % songs.length;
+  loadSong(currentIndex);
+}
+
+loadSong(currentIndex);
+document.getElementById("audio").onended = nextSong;
+
+const audio = document.getElementById("audio");
+const cover = document.getElementById("cover");
+
+audio.addEventListener("play", () => {
+  cover.style.animationPlayState = "running";
+});
+
+audio.addEventListener("pause", () => {
+  cover.style.animationPlayState = "paused";
+});
